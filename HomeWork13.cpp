1caea3e94c24d6769d4bacab892dd781a9dc81eb
Virij80@gmail.com <kbpf2005>
2021-03-15 17:31:43 +0300

    Добавьте файлы проекта.

---
+++ HomeWork13.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream>
+
+// Подключаем созданную библиотеку
+#include "Helpers.h"
+
+int main()
+
+// Присваиваем значение переменным и выводим результат в консоль
+{
+    int result = SquareSum(10, 20);
+    std::cout << "result = " << result << std::endl;
+}
+
